30c54f1e61a0c67fc661004cdef3aa6ea9b862dc
kg534 <kg-katsu08@i.softbank.jp>
2020-05-09 17:23:44 +0900

    user restore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(name: "管理者", email: "admin@gmail.com", password: "aaaaaaa", admin: true)
