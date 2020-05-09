953404bf9cd8c8ee0b5c5522ccb7f3a871882ee8
kg534 <kg-katsu08@i.softbank.jp>
2020-05-09 17:02:08 +0900

    db seed add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "管理者", email: "admin@gmail.com", password: "aaaaaaa", admin: true)
